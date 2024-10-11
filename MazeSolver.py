9b919a71bf847603e25d68e309a3d9a060ff8aa8
Andrzejjp <225070@live.brock.ac.uk>
2024-10-11 09:56:19 +0100

    Add files via upload

---
+++ MazeSolver.py
@@ -0,0 +1,25 @@
+import pygame
+WinSize = (1400,700)
+FPS = 60
+running = True
+clock = pygame.time.Clock()
+pygame.display.init()
+win = pygame.display.set_mode((WinSize[0], WinSize[1]))
+############################################################################################################################
+while running:
+    
+
+
+
+
+
+
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+    clock.tick(FPS)
+pygame.quit()
