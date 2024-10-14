41fbcaba0235c826d5141fb76bb429b35a4cb2ac
AndrzejBrock <225070@brock.live.ac.uk>
2024-10-14 12:20:21 +0100

    literaly nothing

--- Main.py
+++ Main.py
@@ -1,3 +1,6 @@
+##Todo
+#make the grid an object and make the parts of the maze like: "composition"
+
 import pygame
 WinSize = (1400,700)
 FPS = 60
@@ -5,6 +8,7 @@ running = True
 clock = pygame.time.Clock()
 pygame.display.init()
 win = pygame.display.set_mode((WinSize[0], WinSize[1]))
+
 ############################################################################################################################
 while running:
     win.fill((200,0,0))
