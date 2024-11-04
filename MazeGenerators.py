10908a519285f971099faac2b33b9b546b7b6abd
AndrzejBrock <225070@brock.live.ac.uk>
2024-11-04 12:20:53 +0000

    still fixing the maze generator

--- MazeGenerators.py
+++ MazeGenerators.py
@@ -1,14 +1,17 @@
 import random 
 def CheckVisits(grid,pos):
     count = 0
-    if grid.mapArray[pos[0]+1][pos[1]] == 0:
-        count += 1
-    if grid.mapArray[pos[0]-1][pos[1]] == 0:
-        count += 1
-    if grid.mapArray[pos[0]][pos[1]+1] == 0:
-        count += 1
-    if grid.mapArray[pos[0]][pos[1]-1] == 0:
-        count += 1
+    if (pos[0]>0 and pos[0]< grid.rows) and (pos[1]>0 and pos[1]<grid.cols):
+        if grid.mapArray[pos[0]+1][pos[1]] == 0:
+            count += 1
+        if grid.mapArray[pos[0]-1][pos[1]] == 0:
+            count += 1
+        if grid.mapArray[pos[0]][pos[1]+1] == 0:
+            count += 1
+        if grid.mapArray[pos[0]][pos[1]-1] == 0:
+            count += 1
+    else:
+        print("edged")
     if count <= 1:
         return True
     else:
@@ -23,11 +26,13 @@ def DepthFirst(grid,startPos):
     print()
     directionArray = [(1,0),(-1,0),(0,1),(0,-1)]
     grid.mapArray[startPos[0]][startPos[1]] = 0
+
     #Picks a direction to travel in
     while len(directionArray) != 0:
         directionIndex = random.randint(0,len(directionArray)-1)
         directon = directionArray[directionIndex]
         newPos = (startPos[0]+directon[0],startPos[1]+directon[1])
+
         #checks if its in the grid or if already been
         if newPos[0] < grid.rows and newPos[0] >= 0:
             if newPos[1] < grid.cols and newPos[1] >= 0:
