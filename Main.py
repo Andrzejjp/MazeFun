805568a87c9222da284d07e23802c9cb2307f113
AndrzejBrock <225070@brock.live.ac.uk>
2025-01-10 10:29:19 +0000

    gotta fix a few things

--- Main.py
+++ Main.py
@@ -9,10 +9,18 @@ running = True
 clock = pygame.time.Clock()
 pygame.display.init()
 win = pygame.display.set_mode((winSize[0], winSize[1]))
+pygame.display.set_caption("MazeFun")
 
 maze1 = Maze(win,(150,50),10,10)
 visitedList = []
 DepthFirst(maze1,(4,0),visitedList)
+
+stepButton = Button((20,40),(50,20),win,"step")
+count = 1
+end = 0
+for i in maze1.stateString:
+    if i == ".":
+        end += 1
 
 
 
@@ -20,9 +28,14 @@ DepthFirst(maze1,(4,0),visitedList)
 while running:
     DrawStatics(win)
     maze1.DrawMazeThin((255,255,255),(100,200,250))
+    stepButton.Draw()
+    stepButton.RegisterClick()
+    if stepButton.clicked == True and count < end:
+        stepButton.clicked = False
+        maze1.ApplySteps(count)
+        count +=1
 
-
-
+    
 
 
 
